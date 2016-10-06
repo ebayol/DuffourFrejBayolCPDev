c4ff9b17f3f031245f342c072af6b3bafae4849c
frejismail <frejismail>
2016-10-06 15:26:23 +0200

    modifications of web files

--- src/web/supprimer_atelier.php
+++ src/web/supprimer_atelier.php
@@ -19,6 +19,6 @@ $sql = 'DELETE FROM workshop WHERE id_workshop = '.$val.'';
 mysql_query ($sql) or die ('Erreur SQL !'.$sql.'<br />'.mysql_error()); 
 			mysql_close();
 		echo '<p style="color:green;"> La suppression a ete effectue avec succees </p>';
-	echo '<p style="color:red;"><a href=../gestion_atelier.php> Retour </a></p>'; 	
+	echo '<p style="color:red;"><a href=./gestion_atelier.php> Retour </a></p>'; 	
 
 ?>
